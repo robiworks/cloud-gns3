fabf7a96eda18e62c3c2cc02e753fb42f1f95fd3
Robert Samardžija <samardzija.robert@gmail.com>
2021-12-02 16:10:12 +0100

    Install minimal XFCE4 GUI and autologin to vagrant user

--- vagrant/gns3-client.sh
+++ vagrant/gns3-client.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
-# Update package index and upgrade packages
-export DEBIAN_FRONTEND=noninteractive
+# Add GNS3 PPA and update & upgrade package index
 add-apt-repository ppa:gns3/ppa -y
 apt-get update
 apt-get upgrade -y
 
 # Install minimal desktop
-apt-get install lubuntu-desktop -y
+apt-get install xfce4 xorg lightdm firefox -y
+rm /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf
+cat > /usr/share/lightdm/lightdm.conf.d/50-xfce-greeter.conf << EOF
+[SeatDefaults]
+greeter-session=unity-greeter
+user-session=xfce
+allow-guest=false
+autologin-user=vagrant
+autologin-user-timeout=0
+EOF
 
-# Reboot after installing GUI
+# Install GNS3 GUI
+#export DEBIAN_FRONTEND=noninteractive
+
+# Reboot after installing
 reboot
