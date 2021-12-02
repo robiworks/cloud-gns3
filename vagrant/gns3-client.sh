c310f02075212e63e01dd5667fdf35aefa19d7f4
Robert Samardžija <samardzija.robert@gmail.com>
2021-12-02 17:12:44 +0100

    Update box to Bionic 18.04 and finish GUI install

--- vagrant/gns3-client.sh
+++ vagrant/gns3-client.sh
@@ -1,26 +1,19 @@
 #!/usr/bin/env bash
 
 # Add GNS3 PPA and update & upgrade package index
+apt-add-repository multiverse -y
 add-apt-repository ppa:gns3/ppa -y
 apt-get update
 apt-get upgrade -y
 
-# Install minimal desktop
-apt-get install xfce4 xorg lightdm firefox -y
-rm /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf
-cat > /usr/share/lightdm/lightdm.conf.d/50-xfce-greeter.conf << EOF
-[SeatDefaults]
-greeter-session=unity-greeter
-user-session=xfce
-allow-guest=false
-autologin-user=vagrant
-autologin-user-timeout=0
-EOF
+# Install XFCE desktop https://stackoverflow.com/a/53363591
+apt-get install -y xfce4 virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+sed -i 's/allowed_users=.*$/allowed_users=anybody/' /etc/X11/Xwrapper.config
+apt-get install -y lightdm lightdm-gtk-greeter
 
 # Install GNS3 GUI
 export DEBIAN_FRONTEND=noninteractive
-add-apt-repository ppa:gns3/ppa -y
-apt-get install gns3 -y
+apt-get install -y gns3
 
 # Reboot after installing
 reboot
