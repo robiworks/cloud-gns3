5a643d45f0f8854f20e751c209d4d25e51989576
Robert Samardžija <samardzija.robert@gmail.com>
2021-12-04 20:27:58 +0100

    Add working provision script to install GNS3, Docker, setup the user

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -3,4 +3,33 @@
 # Install lightweight desktop environment (XFCE4) and display manager (LightDM)
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y xfce4 lightdm lightdm-gtk-greeter firefox virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+apt-get install -y xfce4 firefox virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+
+# TODO: Remove gnome after install
+
+# Install GNS3 Server and GUI
+# Wireshark hack https://unix.stackexchange.com/q/367866
+echo "wireshark-common wireshark-common/install-setuid boolean true" | debconf-set-selections
+add-apt-repository ppa:gns3/ppa -y
+apt-get update
+apt-get install -y gns3-gui gns3-server
+
+# Install Docker CE
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository -y \
+"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce
+
+# Add user to required groups
+usermod -aG ubridge vagrant
+usermod -aG libvirt vagrant
+usermod -aG kvm vagrant
+dpkg-reconfigure wireshark-common
+usermod -aG wireshark vagrant
+usermod -aG docker vagrant
+
+# echo "autologin-user=vagrant" | tee -a /etc/lightdm/lightdm.conf
+reboot
