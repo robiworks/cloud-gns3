0f235cb6edbdf1542b014d37fabadaa9eb6e68cd
Robert Samardžija <samardzija.robert@gmail.com>
2021-12-05 14:40:02 +0100

    Update provision script and add config files

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-# Install lightweight desktop environment (XFCE4) and display manager (LightDM)
+# Install lightweight desktop environment (XFCE4)
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y xfce4 firefox virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
-
-# TODO: Remove gnome after install
 
 # Install GNS3 Server and GUI
 # Wireshark hack https://unix.stackexchange.com/q/367866
@@ -31,5 +29,15 @@ dpkg-reconfigure wireshark-common
 usermod -aG wireshark vagrant
 usermod -aG docker vagrant
 
-# echo "autologin-user=vagrant" | tee -a /etc/lightdm/lightdm.conf
+# Copy GDM3 autologin config
+cp /vagrant/gdm3.conf /etc/gdm3/custom.conf
+
+# Create GNS3 config folder if it does not exist and change owner, group to vagrant
+mkdir -p /home/vagrant/.config/GNS3/2.2
+chown -R vagrant:vagrant /home/vagrant/.config/GNS3/2.2
+# Copy over GNS3 config files
+cp /vagrant/gns3_gui.conf /home/vagrant/.config/GNS3/2.2/gns3_gui.conf
+cp /vagrant/gns3_server.conf /home/vagrant/.config/GNS3/2.2/gns3_server.conf
+
+# Reboot due to GUI install
 reboot
